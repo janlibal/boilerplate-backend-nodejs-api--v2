81ebbd7a3651d89fd6157ddb05749b4cb6fa92ea
Jan Libal <libalj@seznam.cz>
2024-01-09 15:03:29 +0100

    signUp method implementation

---
+++ src/controllers/userController.ts
@@ -0,0 +1,26 @@
+import { IContext } from "../interfaces/IContext"
+import { IUser } from "../interfaces/IUser"
+
+import userOperations from "../operations/userOperations"
+import validate from "../validations"
+import schema from '../validations/schemas/userSchema'
+
+
+export async function signUp(ctx: IContext){
+
+    const input:IUser = {
+        email: ctx.request.body.email,
+        name: ctx.request.body.name,
+        password: ctx.request.body.password
+    }
+
+    validate(schema.signUp, input)
+
+    const user = await userOperations.create(input)
+
+    ctx.body = {
+        status: 'success',
+        data: user
+    }
+
+}
