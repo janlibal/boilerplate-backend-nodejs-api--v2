9ed36d3cff8aa6f8932f636d39e8721b68425a98
Jan Libal <libalj@seznam.cz>
2024-01-09 15:04:40 +0100

    create user route implementation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,14 @@ const router = new Router()
 const api = new Router()
 
 import testRoutes from './testRoutes'
+import userRoutes from './userRoutes'
+
 
 api.use(testRoutes)
+api.use(userRoutes)
+
 
 router.use('/api/v1', api.routes())
+  
 
 export default router
---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,9 @@
+import Router from "koa-router"
+import { signUp, signIn  } from "../controllers/userController"
+
+const router = new Router()
+
+router.post('/user', signUp)
+router.post('/login',signIn)
+
+export default router.routes()
