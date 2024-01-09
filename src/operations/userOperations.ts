157c5be2b5afda610ca18b35100e6b123410f630
Jan Libal <libalj@seznam.cz>
2024-01-09 15:07:49 +0100

    login method implementation

--- src/operations/userOperations.ts
+++ src/operations/userOperations.ts
@@ -37,6 +37,43 @@ async function create(input: IUser) {
     
 }
 
+async function login(input: IUser) {
+
+    logger.info('login started')
+  
+    const data = {
+        email: input.email.toLowerCase(),
+        password: input.password
+    }
+  
+    const user = await userRepository.findByEmail(data.email)
+  
+    if (!user) {
+        logger.info('Unauthorized')
+        throw new errors.Unauthorized()
+    }
+  
+    const verified = await crypto.comparePasswords(data.password, user.password)
+  
+    if (!verified) {
+        logger.info('Unauthorized')
+        throw new errors.Unauthorized()
+    }
+  
+    const token = await crypto.generateAccessToken(user.id)
+  
+    logger.info('login finished')
+  
+    return {
+        id: user.id,
+        email: user.email,
+        token,
+      }
+  
+  
+  }
+
 export default { 
-    create
+    create,
+    login
 }
