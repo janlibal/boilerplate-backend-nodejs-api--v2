d182c054bb0f0aebfe68aa37c2d78d63183b61d2
Jan Libal <libalj@seznam.cz>
2024-01-09 14:54:22 +0100

    hashPassword method implementation

---
+++ src/utils/crypto.ts
@@ -0,0 +1,30 @@
+import crypto from 'crypto'
+import bcrypt from 'bcryptjs'
+import jwt, { SignOptions, VerifyOptions } from 'jsonwebtoken'
+import config from '../config'
+
+
+const signOptions: SignOptions = {
+    algorithm: config.auth.createOptions.algorithm, 
+    expiresIn: config.auth.createOptions.expiresIn,
+    issuer: `${config.auth.createOptions.issuer}.${config.server.environment}`
+}
+
+
+function hashPassword(password: string) {
+    return bcrypt.hash(peperify(password), config.auth.saltRounds)
+}
+
+
+
+
+function peperify(password: string) {
+    return crypto.createHmac('sha1', config.auth.secret)
+      .update(password)
+      .digest('hex')
+}
+
+
+export default {
+    hashPassword
+}
